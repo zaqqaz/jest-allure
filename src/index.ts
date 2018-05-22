da8d3903c8523c0504953f5152e7b7baa750804a
zaqqaz <i@denis.by>
2018-05-22 13:59:37 +0300

    Add setup config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-class JestAllureReport {
-    constructor(globalConfig: any, options: any) {
+const fs = require("fs");
+
+class JestAllureReport implements jest.Reporter {
+    private _globalConfig: jest.GlobalConfig;
+    private _options: {};
+
+    constructor(globalConfig: jest.GlobalConfig, options: {}) {
         this._globalConfig = globalConfig;
         this._options = options;
     }
 
-    public onRunStart(...props) {
-        console.log(props);
+    public onTestResult(...props) {
+        console.log('Custom reporter output:');
+        // console.log('GlobalConfig: ', this._globalConfig);
+        // console.log('Options: ', this._options)
+        fs.writeFileSync("./test_results.js", JSON.stringify(props));
+    }
+    public onRunComplete(contexts: jest.Set<jest.Context>, results: jest.AggregatedResult) {
+        console.log('Custom reporter output:');
+        // console.log('GlobalConfig: ', this._globalConfig);
+        // console.log('Options: ', this._options)
+        console.log('results: ', results)
+        fs.writeFileSync("./results.js", JSON.stringify(results));
     }
 }
 
