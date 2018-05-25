36d418b9bc977ef9f8bbbc06a1c8f4cb277886fe
Dmitry Bogomya <dmitry.bogomya@gmail.com>
2018-05-25 13:48:07 +0300

    Implement JasmineAllureReporter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import path from "path";
-import Allure from "allure-js-commons";
-import stripAnsi from "strip-ansi";
 
 declare namespace JestAllureReporter {
     type ReporterConfig = {
@@ -9,38 +7,22 @@ declare namespace JestAllureReporter {
 }
 
 class JestAllureReporter implements jest.Reporter {
-    private globalConfig: jest.GlobalConfig;
-    static allure = new Allure();
+    private reporterOptions: JestAllureReporter.ReporterConfig;
 
-    constructor(globalConfig: jest.GlobalConfig, options?: JestAllureReporter.ReporterConfig) {
-        this.globalConfig = globalConfig;
-        const outDir = path.resolve(".", options && options.resultsDir || 'allure-results');
-        JestAllureReporter.allure.setOptions({targetDir: outDir})
+    constructor(globalConfig: jest.GlobalConfig, options: Partial<JestAllureReporter.ReporterConfig> = {}) {
+        this.reporterOptions = { resultsDir: path.resolve(".", options.resultsDir || "allure-results") };
     }
 
-    public onTestResult(test: jest.Test, testResult: jest.TestResult, aggregatedResult: jest.AggregatedResult) {
-        let time = Date.now();
-        for (const result of testResult.testResults) {
-            const currentSuite = JestAllureReporter.allure.getCurrentSuite();
-            const testSuiteName = result.ancestorTitles.join(" ");
-            if (!currentSuite) {
-                JestAllureReporter.allure.startSuite(testSuiteName, time);
-            } else if (currentSuite.name !== testSuiteName) {
-                JestAllureReporter.allure.endSuite(time);
-                JestAllureReporter.allure.startSuite(testSuiteName, time);
-            }
-            JestAllureReporter.allure.startCase(result.title, time);
-            time += result.duration || 0;
-            JestAllureReporter.allure.endCase(
-                result.status,
-                result.failureMessages.length ? {message: stripAnsi(result.failureMessages[0])} : undefined,
-                time
-            );
+    onTestStart(test: jest.Test) {
+        const setupPath = require.resolve('./setup');
+        const setupTestFrameworkScriptFile = test.context.config.setupTestFrameworkScriptFile;
+        if (setupTestFrameworkScriptFile && setupTestFrameworkScriptFile !== setupPath) {
+            throw new Error(`
+                You have already registered your own "setupTestFrameworkScriptFile" in Jest config.
+                Please refer to the documentation of "jest-allure" package and update your setup file accordingly.
+            `);
         }
-
-        if(JestAllureReporter.allure.getCurrentSuite()) {
-            JestAllureReporter.allure.endSuite(time);
-        }
+        test.context.config = { ...test.context.config, setupTestFrameworkScriptFile: setupPath }
     }
 }
 
---
+++ src/setup.ts
@@ -0,0 +1,48 @@
+import Allure from "allure-js-commons";
+import stripAnsi from "strip-ansi";
+
+class JasmineAllureReporter implements jasmine.CustomReporter{
+    private allure: Allure;
+
+    constructor(allure: Allure) {
+        this.allure = allure;
+    }
+
+    suiteStarted(suite: jasmine.CustomReporterResult) {
+        this.allure.startSuite(suite.fullName);
+    }
+
+    suiteDone() {
+        this.allure.endSuite();
+    };
+
+    specStarted(spec: jasmine.CustomReporterResult) {
+        this.allure.startCase(spec.description);
+    };
+
+    specDone(spec: jasmine.CustomReporterResult) {
+        let error;
+        if (spec.status === "pending") {
+            error = { message: spec.pendingReason };
+        }
+        if (spec.status === "disabled") {
+            error = { message: "This test was disabled" };
+        }
+        const failure = spec.failedExpectations && spec.failedExpectations.length ? spec.failedExpectations[0] : undefined;
+        if (failure) {
+            error = {
+                message: stripAnsi(failure.message),
+                stack: stripAnsi(failure.stack)
+            };
+        }
+
+        this.allure.endCase(spec.status as jest.Status, error);
+    };
+}
+
+export function registerAllureReporter() {
+    const allure = (global as any).allure = new Allure();
+    jasmine.getEnv().addReporter(new JasmineAllureReporter(allure));
+}
+
+registerAllureReporter();
