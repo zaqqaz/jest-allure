14fbc048383ff96288e273e7140c9dcaf579a982
Denis Artyuhovich <i@denis.by>
2018-06-05 19:05:43 +0300

    Add chaining to reporter

--- src/Reporter.ts
+++ src/Reporter.ts
@@ -25,49 +25,61 @@ export class Reporter {
 
     public description(description: string) {
         this.allure.setDescription(description);
+        return this;
     }
 
     public severity(severity: Severity) {
         this.addLabel('severity', severity);
+        return this;
     }
 
     public epic(epic: string) {
         this.addLabel('epic', epic);
+        return this;
     }
 
     public feature(feature: string) {
         this.addLabel('feature', feature);
+        return this;
     }
 
     public story(story: string) {
         this.addLabel('story', story);
+        return this;
     }
 
     public startStep(name: string) {
         this.allure.startStep(name);
+        return this;
     }
 
     public endStep(status: Status = Status.Passed) {
         this.allure.endStep(status);
+        return this;
     }
 
     public addArgument(name: string) {
         this.allure.startStep(name);
+        return this;
     }
 
     public addEnvironment(name: string, value: string) {
         this.allure.getCurrentTest().addParameter('environment-variable', name, value);
+        return this;
     }
 
     public addAttachment(name: string, buffer: any, type: string) {
         this.allure.addAttachment(name, buffer, type);
+        return this;
     }
 
     public addLabel(name: string, value: string) {
         this.allure.getCurrentTest().addLabel(name, value);
+        return this;
     };
 
     public addParameter(paramName: string, name: string, value: string) {
         this.allure.getCurrentTest().addParameter(paramName, name, value);
+        return this;
     };
 }
