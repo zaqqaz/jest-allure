a9136c4a96ab3a17e8f248844d75d63898f452d8
Denis Artyuhovich <i@denis.by>
2018-06-13 17:37:10 +0300

    Add example with description, steps and screenshot

---
+++ examples/__tests__/test1.js
@@ -0,0 +1,38 @@
+const { Severity } = require("jest-allure/dist/Reporter");
+
+const timeout = 5000;
+
+describe(
+    '/ (Home Page)',
+    () => {
+        let page;
+
+        beforeAll(async () => {
+            page = await global.__BROWSER__.newPage();
+            await page.goto('https://google.com');
+        }, timeout);
+
+        afterAll(async () => {
+            await page.close();
+        });
+
+        it('should load without error', async () => {
+            reporter
+                .description("Home Page test suite")
+                .story("GOOGL-01")
+                .severity(Severity.Critical);
+
+            const text = await page.evaluate(() => document.body.textContent);
+
+            reporter.startStep("Check that home page contain google");
+            expect(text).toContain('google');
+            reporter.endStep();
+
+            reporter.startStep("Make a screenshot");
+            const screenshot = await page.screenshot();
+            reporter.addAttachment("Home Page", screenshot, "image/png");
+            reporter.endStep();
+        })
+    },
+    timeout
+);
---
+++ examples/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    globalSetup: './setup.js',
+    globalTeardown: './teardown.js',
+    testEnvironment: './puppeteer_environment.js',
+    reporters: ["default", "jest-allure"],
+};
